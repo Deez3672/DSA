02f8623aabedf1bbae6beec798cd286b101a0472
Deesara <149193756+Deez3672@users.noreply.github.com>
2024-10-25 22:50:28 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+public class {
+
+    public void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {8, 3, 1, 4, 5, 11, 7, 2, 17, 13};
+        System.out.println("Original array:");
+        printArray(numbers);
+
+        BubbleSort sorter = new BubbleSort();
+        sorter.bubbleSort(numbers);
+
+        System.out.println("Sorted Array:");
+        printArray(numbers);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
